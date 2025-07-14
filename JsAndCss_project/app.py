b6b5ab45ab64d6c8ecc5f0fb20fbcc05f0c3350a
Eduardo <dudulazzari08@gmail.com>
2025-07-14 17:36:32 -0300

    Terminando o site

--- JsAndCss_project/app.py
+++ JsAndCss_project/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, flash, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, make_response, session
 
 app = Flask(__name__)
 app.secret_key = 'edudela'
@@ -15,16 +15,23 @@ def registrarUsuario():
         password = request.form['password']
 
 
+        
+
         # Validação simples do lado do servidor
         if not username or not email or not password:
             flash('Todos os campos são obrigatórios', 'danger')
         else:
             flash(f'Usuário {username} cadastrado com sucesso!', 'success')
-            return redirect(url_for('registrarUsuario'))
+            if "usuarios" in session:
+                session["usuarios"].append([username, email, password])
+
+            else:
+                session["usuarios"] = [[username, email, password]]
+            cadastro = session['usuarios']
+            return render_template('formulario.html', cadastro=cadastro)
 
         # flash(): Armazena uma mensagem que será exibida na próxima requisição. Isso
         # é útil para fornecer mensagens ao usuário.
-
     return render_template('formulario.html')
                            
 if __name__ == '__main__':
